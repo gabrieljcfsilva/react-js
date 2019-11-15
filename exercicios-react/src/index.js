fcbaec12229d9d41a7eed19a35f7abcd39174fe5
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-14 21:37:20 -0300

    primeiro componente

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,15 +1,11 @@
 import ReactDom from 'react-dom'
 import React from 'react'
 
-function getList() {
-    return (
-        <ul>
-            <li>1) Bruno</li>
-            <li>1) Maria</li>
-            <li>1) Jose</li>
-        </ul>
-    )
-}
+import PrimeiroComponente from './components/PrimeiroComponente'
 
 const element = document.getElementById('root')
-ReactDom.render(getList(), element)
+ReactDom.render(
+    <>
+        <PrimeiroComponente />
+    </>
+    , element)
