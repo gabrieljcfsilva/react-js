6eef1fb3482a480f93988e32d7e6c9cccc4a3dc8
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-14 21:19:51 -0300

    conhecendo mais sobre JSX

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,6 +1,15 @@
 import ReactDom from 'react-dom'
 import React from 'react'
 
+function getList() {
+    return (
+        <ul>
+            <li>1) Bruno</li>
+            <li>1) Maria</li>
+            <li>1) Jose</li>
+        </ul>
+    )
+}
 
 const element = document.getElementById('root')
-ReactDom.render(<h1>Olá React</h1>, element)
+ReactDom.render(getList(), element)
