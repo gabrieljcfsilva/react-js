15282315dbf5081bad2b8af3829b7c50469332a2
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-14 22:59:03 -0300

    comunicação entre componentes

---
+++ exercicios-react/src/components/FamiliaSilva.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Membro from './Membro'
+
+export default props =>
+    <div>
+        <Membro nome="Rafael" sobrenome="Silva" />
+        <Membro nome="Daniela" sobrenome="Silva" />
+        <Membro nome="Carla" sobrenome="Silva" />
+        <Membro nome="Pedro" sobrenome="Silva" />
+    </div>
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,16 +2,24 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 // import PrimeiroComponente from './components/PrimeiroComponente'
-import CompA, { CompB as B } from './components/DoisComponentes'
+// import CompA, { CompB as B } from './components/DoisComponentes'
 // import MultElementos from './components/MultElementos'
+// import FamiliaSilva from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Membro from './components/Membro'
 
 const element = document.getElementById('root')
 
 ReactDom.render(
     <>
+        <Familia>
+            <Membro nome="Andre" sobrenome="Pereira" />
+            <Membro nome="Carla" sobrenome="Pereira" />
+        </Familia>
         {/* <PrimeiroComponente valor="Bom dia!" abcd={123**2}/> */}
         {/* <MultElementos /> */}
-        <CompA valor="Componente A" />
-        <B valor="Componente B" />
+        {/* <CompA valor="Componente A" />
+        <B valor="Componente B" /> */}
+        {/* <FamiliaSilva /> */}
     </>
     , element)
