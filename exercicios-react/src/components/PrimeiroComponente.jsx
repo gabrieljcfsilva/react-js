65e0317eb446e96e527e9c021b2275329d84486e
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-14 21:42:59 -0300

    componente com arrow function

--- exercicios-react/src/components/PrimeiroComponente.jsx
+++ exercicios-react/src/components/PrimeiroComponente.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente</h1>
-}
+export default () => <h1>Primeiro Componente (Arrow)!</h1>
+
+
+// export default function() {
+//     return <h1>Primeiro Componente</h1>
+// }
 
 
 // function primeiro() {
