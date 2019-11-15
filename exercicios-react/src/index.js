d976d418a629fde0bcc4a2a1f0ee4bc54e407489
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-14 22:21:53 -0300

    um arquivo multiplos componentes

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,10 +2,12 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import PrimeiroComponente from './components/PrimeiroComponente'
-
+import { CompA, CompB as B } from './components/DoisComponentes'
 const element = document.getElementById('root')
 ReactDom.render(
     <>
         <PrimeiroComponente valor="Bom dia!" abcd={123**2}/>
+        <CompA valor="Componente A"/>
+        <B valor="Componente B"/>
     </>
     , element)
