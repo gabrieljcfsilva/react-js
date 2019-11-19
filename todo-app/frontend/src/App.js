e0bda7ead007bfaa3c8e30db77875b928cbb88dd
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-19 13:04:34 -0300

    criando componente todo e about

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -3,8 +3,12 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 
+import Todo from './components/Todo'
+import About from './components/About'
+
 export default props => (
     <div className="container">
-        <h1>Hello React</h1>
+        <Todo />
+        <About />
     </div>
 )
