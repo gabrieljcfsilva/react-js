7669f11292476f5312358a66fec4ee1beea28480
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-22 02:20:10 -0300

    criando state e evento de input

--- todo-app/frontend/src/components/Todo.jsx
+++ todo-app/frontend/src/components/Todo.jsx
@@ -7,17 +7,28 @@ import TodoList from './TodoList'
 export default class Todo extends Component {
     constructor(props) {
         super(props)
+        this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
+
     handleAdd() {
-        console.log('Add')
+        console.log(this.state.description)
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
     }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}
+                />
                 <TodoList />
             </div>
         )
--- todo-app/frontend/src/components/TodoForm.jsx
+++ todo-app/frontend/src/components/TodoForm.jsx
@@ -6,11 +6,14 @@ import IconButton from './IconButton'
 export default props => (
     <div role="form" className="todoForm">
         <Grid cols="12 9 10 11">
-            <input type="text" id="description" className="form-control" placeholder="Adicione uma tarefa" />
+            <input type="text" id="description" className="form-control"
+                placeholder="Adicione uma tarefa"
+                onChange={props.handleChange}
+                value={props.description} />
         </Grid>
 
         <Grid cols="12 3 2 1">
-            <IconButton styleButton="primary" icon="plus" onClick={props.handleAdd}/>
+            <IconButton styleButton="primary" icon="plus" onClick={props.handleAdd} />
         </Grid>
     </div>
 )
