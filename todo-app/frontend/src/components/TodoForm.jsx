1f23d7a431f64164ec387c9557c4674629495ca4
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-25 15:15:00 -0300

    criando atalhos dos botões

--- todo-app/frontend/src/components/TodoForm.jsx
+++ todo-app/frontend/src/components/TodoForm.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 import Grid from './Grid'
 import IconButton from './IconButton'
 
-export default props => (
-    <div role="form" className="todoForm">
+export default props => {
+    const keyHandler = e => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (<div role="form" className="todoForm">
         <Grid cols="12 9 10">
             <input type="text" id="description" className="form-control"
                 placeholder="Adicione uma tarefa"
                 onChange={props.handleChange}
+                onKeyUp={keyHandler}
                 value={props.description} />
         </Grid>
 
@@ -20,5 +29,5 @@ export default props => (
             <IconButton styleButton="default" icon="close"
                 onClick={props.handleClear} />
         </Grid>
-    </div>
-)
+    </div>)
+}
